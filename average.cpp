7c54d3ee038915c7150bf42acbdb06de52fd7266
FirstStepHub <first_yo1@hotmail.com>
2026-03-06 10:05:08 +0700

    average_complete.cpp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <string>
 
+using namespace std ;
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        cout << "Please input numbers to find average." << endl ;
+        return 0 ;
+    }
+
+    double sum = 0.0 ;
+    int count = argc - 1 ;
+
+    for (int i = 1; i < argc; ++i) {
+        try {
+            sum += stod(argv[i]) ;
+        } catch (const exception& e) {
+            cout << "Invalid input: " << argv[i] << endl ;
+            return 1 ;
+        }
+    }
+
+    double average = sum / count ;
+
+    cout << "---------------------------------" << endl ;
+    cout << "Average of " << count << " numbers = " << average << endl ;
+    cout << "---------------------------------" << endl ;
+
+    return 0 ;
+}
